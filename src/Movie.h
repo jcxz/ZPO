1e39c68de45efb95c7c3b4c29be97aec9ceaad31
jcxz <xfedor01@stud.fit.vutbr.cz>
2014-07-06 00:32:26 +0200

    LGPL license

--- src/Movie.h
+++ src/Movie.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2014 Matus Fedorko <xfedor01@stud.fit.vutbr.cz>
+ *
+ * This file is part of ZPO.
+ *
+ * ZPO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ZPO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ZPO. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef MOVIE_H
 #define MOVIE_H
 
@@ -37,7 +56,7 @@ class Movie
     int frameCount(void) const { return m_frames.size(); }
     int frameWidth(void) const { return m_frame_w; }
     int frameHeigth(void) const { return m_frame_h; }
-    const QImage & frame(int idx) { return m_frames[idx]; }
+    const QImage & frame(int idx) const { return m_frames[idx]; }
 
     bool addFrame(const QImage & frame)
     {
